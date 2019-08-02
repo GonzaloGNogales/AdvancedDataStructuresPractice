26fafebe66130a5a7c4cf65ebaab0f8784a33538
GonzaloGNogales <gonzalognbusiness@gmail.com>
2019-08-02 15:40:17 +0200

    Complete LinkedList implementation finished

--- src/edalearning/EDALearning.java
+++ src/edalearning/EDALearning.java
@@ -1,5 +1,4 @@
 package edalearning;
-
 /**
  *
  * @author Gonzalo Gómez Nogales
@@ -7,8 +6,42 @@ package edalearning;
 
 public class EDALearning {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
+        LinkedList<Integer> l = new LinkedList<>();
         
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        l.add(6);
+        l.add(7);
+        l.add(7);
+        l.add(7);
+        l.add(7);
+        l.show();
+        System.out.println();
+        System.out.println();
+        
+        System.out.println(l.get());
+        System.out.println(l.size());
+        
+        System.out.println();
+        System.out.println();
+        l.remove();
+        l.remove();
+        l.remove();
+        l.remove();
+        l.remove();
+        l.show();
+        
+        System.out.println();
+        System.out.println();
+        System.out.println(l.search(7));
+        System.out.println(l.contains(7));
+        
+        System.out.println();
+        System.out.println();
     }
 
 }
--- src/edalearning/LinkedList.java
+++ src/edalearning/LinkedList.java
@@ -22,14 +22,53 @@ public class LinkedList<E> implements List<E> {
     
     @Override
     public void add (E value) {
-        Node newNode = new Node(value,this.head);
-        this.head = newNode;
+        Node newNode = new Node(value,null);
+        
+        if (this.isEmpty()) {
+            this.head = newNode;
+        }
+        else {
+            Node aux = this.head;
+            
+            while (aux.getNext() != null) {
+                aux = aux.getNext();
+            }
+            
+            aux.setNext(newNode);
+        }
+
         this.size++;
     }
     
+    //Review
     @Override
     public void add (int index, E value) {
+        Node actual = new Node(this.head);
+        Node anterior = new Node(this.head);
+        int i = 0;
         
+        if (index == i) {
+            Node newNode = new Node(value,this.head);
+            this.head = newNode;
+            this.size++;
+        }
+        else {
+            actual = actual.getNext();
+            i++;
+        }
+        
+        while (i < size) {
+            if (index == i) {
+                Node newNode = new Node(value,actual);
+                anterior.setNext(newNode);
+                this.size++;
+            }
+            else {
+                anterior = anterior.getNext();
+                actual = actual.getNext();
+                i++;
+            }
+        }
     }
     
     @Override
@@ -40,6 +79,7 @@ public class LinkedList<E> implements List<E> {
         return elem;
     }
     
+    //Review
     @Override
     public E remove (int index) {
         Node actual = new Node(this.head);
@@ -85,6 +125,7 @@ public class LinkedList<E> implements List<E> {
         return (E) this.head.getElem();
     }
     
+    //Review
     @Override
     public E get (int index) {
         Node aux = new Node(this.head);
@@ -92,9 +133,63 @@ public class LinkedList<E> implements List<E> {
         int i = 0;
         
         while (i < this.size) {
-            
+            if (i == index) {
+                elem = (E) aux.getElem();
+            }
+            else {
+                aux = aux.getNext();
+                i++;
+            }
         }
         
         return elem;
+    }
+    
+    @Override
+    public int search (E value) {
+        Node aux = new Node(this.head);
+        int i = 0;
+        int contador = 0;
+        
+        while (i < this.size) {
+            if (value == aux.getElem()) {
+                contador++;
+            }
+            
+            aux = aux.getNext();
+            i++;
+        }
+        
+        return contador;
+    }
+    
+    @Override
+    public boolean contains (E value) {
+        Node aux = new Node(this.head);
+        int i = 0;
+        boolean contenido = false;
+        
+        while (i < this.size) {
+            if (value == aux.getElem()) {
+                contenido = true;
+            }
+            
+            aux = aux.getNext();
+            i++;
+        }
+        
+        return contenido;
+    }
+    
+    public void show () {
+        Node aux = new Node(this.head);
+        int i = 0;
+        
+        while (i < this.size) {
+            System.out.println(aux.getElem());
+            aux = aux.getNext();
+            i++;
+        }
+        
     }
 }
